ee5be1a3792a9133b879a24aaf5d60df2014fccf
lw <lw>
2025-11-28 14:11:30 +0100

    update env template

--- env.py
+++ env.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 """Variables and secrets."""
+# UCS
 UCS_HOST = "ucs.domain.com"
 UCS_USERNAME = "admin"
 UCS_PASSWORD = "admin"
 UCS_SESSION_OPTS = {"secure": True}
 
+# MAIL
+MAIL_SMTP_HOST = "mail.domain.com"
+MAIL_FROM_ADDR = "ucs@domain.com"
+MAIL_TO_ADDR = "monitor@domain.com"
