8cd7c2de822750fb9a48f4c80060c813f1a7196e
lw <lw>
2025-12-04 12:50:11 +0100

    feat: check faults and alert via email

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # ucs-threshold-monitor
-Monitors Cisco UCS threshold-crossed events
+
+Monitors Cisco UCS threshold-crossed events and alerts via email.
+
+Since `threshold-crossed` events do [not trigger call-home alerts](https://bst.cisco.com/quickview/bug/CSCwf25809), this will do it for you.
+
+## Install
+
+Get it
+```bash
+git clone https://github.com/lwarnt/ucs-threshold-monitor.git
+cd ucs-threshold-monitor
+```
+
+Install requirements
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Create the service file
+
+```bash
+wd=$(pwd)
+cat >/etc/systemd/system/ucs_threshold_monitor.service <<EOF
+[Unit]
+Description=UCS Threshold Crossed Monitor
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=$wd
+ExecStart=$wd/venv/bin/python $wd/monitor.py
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod +x monitor.py
+
+systemctl daemon-reload
+systemctl enable ucs_threshold_monitor.service
+systemctl start ucs_threshold_monitor.service
+```
+
+## UCS configuration example
+
+```
+Servers > Policies > root > Threshold Policies > thr-policy-default 
+    Add > Create Threshold Class
+        Choose Statistics Class
+            > processor
+            > Cpu Env Stats
+    Next >
+        Threshold Definitions
+            Add > Cpu Env Stats Temperature Avg
+                > Major: UP=75,DOWN=70
+```
---
+++ monitor.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""
+Monitors Cisco UCS threshold-crossed events.
+
+Retention Policy:
+    Admin > All > Faults, Events and Audit Log > Settings > Global Fault Policy
+"""
+import smtplib
+import sys
+from dataclasses import dataclass
+from email.message import EmailMessage
+from time import sleep
+
+from ucsmsdk.ucshandle import UcsHandle
+
+from env import (
+    MAIL_FROM_ADDR,
+    MAIL_SMTP_HOST,
+    MAIL_TO_ADDR,
+    UCS_HOST,
+    UCS_PASSWORD,
+    UCS_SESSION_OPTS,
+    UCS_USERNAME,
+)
+
+
+def send_mail(subject: str, message: str) -> None:
+    """Sends SMTP MIMEmultipart message."""
+    msg = EmailMessage()
+    html_content = (
+        f"<h3>{subject}</h3>"
+        f"<a href='https://{UCS_HOST}/app/ucsm/index.html'>Check here to got to UCSM!</a>"
+        f"<pre>{message}</pre></br>"
+    )
+    msg.add_alternative(html_content, subtype="html")
+    msg["To"] = MAIL_TO_ADDR
+    msg["CC"] = MAIL_TO_ADDR
+    msg["Subject"] = subject
+
+    with smtplib.SMTP(host=MAIL_SMTP_HOST, port=25, timeout=15) as smtp:
+        smtp.sendmail(MAIL_FROM_ADDR, MAIL_TO_ADDR, msg.as_string())
+
+    print(f"mailed: '{msg['Subject']}' to {MAIL_TO_ADDR}")
+
+
+@dataclass(frozen=True)
+class Fault:
+    id: int
+    raised: bool
+    msg: str
+
+    def __hash__(self):
+        return hash(self.id)
+
+
+def monitor_faults():
+    """Query UCS for faults."""
+    session: UcsHandle = None
+    raised_faults: set[int] = set()
+    cleared_faults: set[int] = set()
+
+    try:
+        while True:
+            if session is None:
+                session = UcsHandle(
+                    UCS_HOST, UCS_USERNAME, UCS_PASSWORD, **UCS_SESSION_OPTS
+                )
+                session.login()
+                print(f"new session for {UCS_HOST}")
+
+            print("check faults")
+            faults = session.query_classid(
+                class_id="faultInst",
+                filter_str="(cause, 'threshold-crossed', type='eq')",
+            )
+
+            if len(faults) > 0:
+                for mo in faults:
+                    # ManagedObject https://ciscoucs.github.io/ucsmsdk_docs/ucsmsdk.html#ucsmsdk.ucsmo.ManagedObject
+                    xml = mo.to_xml()
+
+                    raised = True if xml.attrib["severity"] != "cleared" else False
+                    fid = xml.attrib["id"]
+                    dn = xml.attrib["dn"]
+
+                    f = Fault(fid, raised, str(mo))
+
+                    subject = f"UCS - threshold-crossed - {'Raised' if raised else 'Recovered'}: {f.id} - {dn}"
+                    if raised:
+                        if f.id not in raised_faults:
+                            print(f"raised: {f.id}")
+                            send_mail(subject, f.msg)
+                            raised_faults.add(f.id)
+                    else:
+                        if f.id not in cleared_faults:
+                            print(f"cleared: {f.id}")
+                            send_mail(subject, f.msg)
+                            cleared_faults.add(f.id)
+
+            else:
+                raised_faults.clear()
+                cleared_faults.clear()
+
+            sleep(30)
+
+    except Exception as err:
+        print(err)
+
+    finally:
+        session.logout()
+
+
+if __name__ == "__main__":
+    sys.exit(monitor_faults())
